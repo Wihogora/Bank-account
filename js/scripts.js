e5687f1c38c7d160e7c48c8e0b09d4a786b84a84
Wihogora Wivine <wwihogora@gmail.com>
2019-01-19 23:17:37 +0200

    .

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,28 @@ $(document).ready(function(){
     $("button#buttonhide").click(function(){
         $(".container1").hide();
         $(".container2").show();
-        
-    
-
+ 
 var deposit=$("#name1").val();
 console.log(deposit);
-$("#guteranya").text(deposit);
+$("#adding").text(deposit);
 
 var deposit1=$("#initialamount").val();
 console.log(deposit1);
-$("#urakibura").text(deposit1);
+$("#adding1").text(deposit1);
 if ($("button#amount9")) {
 
 	$("button#amount9").click(function(){
     var deposit2 = parseInt($('#amount1').val());
     var sum = deposit2 + parseInt(deposit1);
-    $('#urakibura').text(sum);
+    $('#adding1').text(sum);
 })
-    
 
 }
-
-
-
-// $("button#amount10").click(function(){
-//     var withdraw2 = parseInt($('#amount777').val());
-//     var sum = deposit2 + parseInt(deposit1);
-//     $('#urakibura').text(sum);
 
 $("button#amount10").click(function(){
     var deposit24 = parseInt($('#amount777').val());
     var sum = parseInt(deposit1)-deposit24;
-    $('#urakibura').text(sum);
+    $('#adding1').text(sum);
 
 
 });
